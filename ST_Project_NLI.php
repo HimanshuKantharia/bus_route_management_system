e425573bb6ba4b0c218e1f6da218160259a44277
Samridh Tuladhar <tsamridh86@users.noreply.github.com>
2016-04-30 13:00:17 +0530

    these files have the database modifications
    for some reasons only your login page has file extenstions
    everything else is still working on file management
    this code has been resent to you for the changes.

--- ST_Project_NLI.php
+++ ST_Project_NLI.php
@@ -1,32 +1,40 @@
 <title>ST_Project_NLI</title>
 <?php
-
+	
 	/*	Checking login authority 
 		Login failure will display warning messages and redirects to login page.
-		username to access : admin
-		password : baby
+		the usernames and thier respective passwords have been saved inside a table
+		relation: users(userName,pswd,authority)
 	*/
 	
 	$username = $_POST["user"];
 	$password = $_POST["pswd"];
 	$url = "ST_Project_loginpage.php";
-	if($username != "admin")
+	$connect = new mysqli("localhost","root","","st1_project");
+	$adminData = "select userName , pswd from users where authority = 'A';";
+	$userData  = "select userName, pswd from users where authority = 'U';";
+	$adminData = $connect->query($adminData);
+	$userData = $connect->query($userData);
+	while($row=$adminData->fetch_assoc())
 	{
-		echo
-			"
-				<p>You are not authorized to access this page.</p>
-				<a href = '$url'>Click here</a>
-				<p>To be directed to the login page</p>
-			";
+		if($row["userName"]==$username && $row["pswd"]==$password)
+		{
+			$_SESSION['user']="admin";
+			$_SESSION['pswd']="okay";
+			header("location:ST_Project_Adminpage.php");
+		}
 	}
-	else if($password != "baby")
+	while($row1=$userData->fetch_assoc())
 	{
-		echo
-			"
-				<p>You have entered the incorrect password</p>
-				<a href = '$url'>Click here</a>
-				<p>To be redirected to the login page</p>
-			";
+		if($row1["userName"]==$username && $row1["pswd"]==$password)
+		{
+			
+			$_SESSION['user']="user";
+			$_SESSION['pswd']="okay";
+			header("location:ST_Project_Userpage.php");
+			
+		}
 	}
-	else header("location:ST_Project_Adminpage.php");
+	echo "You are not authorized to access this page. Click <a href = '$url'>here</a> to be redirected.";
+	$connect->close();
 ?>
